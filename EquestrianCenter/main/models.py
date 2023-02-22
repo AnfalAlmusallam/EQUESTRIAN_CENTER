c7bbe8496cedf84770447533d67f1773a6c33607
Anfal Ali <name@gmail.com>
2023-02-22 13:17:04 +0300

    create class Club

--- EquestrianCenter/main/models.py
+++ EquestrianCenter/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+'''Create class named Club and its objects '''
+class Club(models.Model):
+
+    image= models.ImageField(upload_to="images/%y/%m/%d")
+    club_name= models.CharField(max_length=10)
+    club_services= models.TextField()
+    price=models.DecimalField(max_digits=5,decimal_places=2)
+    time_open_closed=models.TimeField()
